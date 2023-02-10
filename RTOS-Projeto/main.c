8400b03bfdebbfef07fb2005ab23114e0690d35f
André Escarião <andre.escariao1@gmail.com>
2023-02-09 23:06:54 -0300

    Update main.c

--- RTOS-Projeto/main.c
+++ RTOS-Projeto/main.c
@@ -1,103 +1,132 @@
 /*
+	FreeRTOS V9.0.0 - Copyright (C) 2016 Real Time Engineers Ltd.
+	All rights reserved
+
+	VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
+
+	This file is part of the FreeRTOS distribution.
+
+	FreeRTOS is free software; you can redistribute it and/or modify it under
+	the terms of the GNU General Public License (version 2) as published by the
+	Free Software Foundation >>!AND MODIFIED BY!<< the FreeRTOS exception.
+
+	***************************************************************************
+	>>!   NOTE: The modification to the GPL is included to allow you to     !<<
+	>>!   distribute a combined work that includes FreeRTOS without being   !<<
+	>>!   obliged to provide the source code for proprietary components     !<<
+	>>!   outside of the FreeRTOS kernel.                                   !<<
+	***************************************************************************
+
+	FreeRTOS is distributed in the hope that it will be useful, but WITHOUT ANY
+	WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+	FOR A PARTICULAR PURPOSE.  Full license text is available on the following
+	link: http://www.freertos.org/a00114.html
+
+	http://www.FreeRTOS.org/FAQHelp.html - Having a problem?  Start by reading
+	the FAQ page "My application does not run, what could be wrong?".  Have you
+	defined configASSERT()?
+
+	http://www.FreeRTOS.org/support - In return for receiving this top quality
+	embedded software for free we request you assist our global community by
+	participating in the support forum.
+
+	http://www.FreeRTOS.org/training - Investing in training allows your team to
+	be as productive as possible as early as possible.  Now you can receive
+	FreeRTOS training directly from Richard Barry, CEO of Real Time Engineers
+	Ltd, and the world's leading authority on the world's leading RTOS.
+
+	http://www.FreeRTOS.org/plus - A selection of FreeRTOS ecosystem products,
+	including FreeRTOS+Trace - an indispensable productivity tool, a DOS
+	compatible FAT file system, and our tiny thread aware UDP/IP stack.
+
+	http://www.FreeRTOS.org/labs - Where new FreeRTOS products go to incubate.
+	Come and try FreeRTOS+TCP, our new open source TCP/IP stack for FreeRTOS.
+
+	http://www.OpenRTOS.com - Real Time Engineers ltd. license FreeRTOS to High
+	Integrity Systems ltd. to sell under the OpenRTOS brand.  Low cost OpenRTOS
+	licenses offer ticketed support, indemnification and commercial middleware.
+
+	http://www.SafeRTOS.com - High Integrity Systems also provide a safety
+	engineered and independently SIL3 certified version for use in safety and
+	mission critical applications that require provable dependability.
+
+	1 tab == 4 spaces!
+*/
+
+/*
 	Projeto disciplina Sistemas em Tempo Real
-	Aluno: AndrÈ Escari„o		MatrÌcula:119210793
-	Aluno: Eduardo S·tiro		MatrÌcula:118210825
+	Aluno: Andr√© Escari√£o		Matr√≠cula:119210793
+	Aluno: Eduardo S√°tiro		Matr√≠cula:118210825
 
 	Tarefas do Sistema:
 
-	Tarefa 1: IrrigaÁ„o do jardim
-	Tarefa 2: IluminaÁ„o da ·rea externa
-	Tarefa 3: AlimentaÁ„o do pet automatizada
-	Tarefa 4: Port„o do pet
+	Tarefa 1: Irriga√ß√£o do jardim
+	Tarefa 2: Ilumina√ß√£o da √°rea externa
+	Tarefa 3: Alimenta√ß√£o do pet automatizada
+	Tarefa 4: Port√£o do pet
 	Tarefa 5: Ligar o ar-condicionado
 	Tarefa 6: Checagem de portas e janelas
 
 */
 
 
-#include 
+#include "Stdio.h"
 /* FreeRTOS.org includes. */
 #include "FreeRTOS.h"
 #include "task.h"
-
+#include "timers.h"
+#include "queue.h"
+//#include "config.h"
+#include <conio.h>
 /* Demo includes. */
 #include "supporting_functions.h"
 
-/* Used as a loop counter to create a very crude delay. */
-#define mainDELAY_LOOP_COUNT		( 0xffffff )
+/* Seis fun√ß√µes de tarefas. */
+void vTask_1(void* pvParameters);
+void vTask_2(void* pvParameters);
+void vTask_3(void* pvParameters);
+void vTask_4(void* pvParameters);
+void vTask_5(void* pvParameters);
+void vTask_6(void* pvParameters);
 
-/* The task functions. */
-void vTask1( void *pvParameters );
-void vTask2( void *pvParameters );
+/* Used to hold the handle of Task2. */
+TaskHandle_t xTask2Handle;
 
 /*-----------------------------------------------------------*/
 
-int main( void )
+void HelloTask(){
+	while (1) {
+		printf("Hello STR\n");
+		vTaskDelay(1000);
+}
+}
+
+
+/* MAIN */ 
+int main(void)
 {
-	/* Create one of the two tasks. */
-	xTaskCreate(	vTask1,		/* Pointer to the function that implements the task. */
-					"Task 1",	/* Text name for the task.  This is to facilitate debugging only. */
-					1000,		/* Stack depth - most small microcontrollers will use much less stack than this. */
-					NULL,		/* We are not using the task parameter. */
-					1,			/* This task will run at priority 1. */
-					NULL );		/* We are not using the task handle. */
+	/*  */
+	xTaskHandle HT;
 
-	/* Create the other task in exactly the same way. */
-	xTaskCreate( vTask2, "Task 2", 1000, NULL, 1, NULL );
+	/* Tarefa de prioridade 1 (menor) */
+	xTaskCreate(HelloTask, "HelloTask", 1000, NULL, 1, &HT);
+	
+	/* Tarefa com prioridade 3 (maior) */
+	xTaskCreate(vTask_1, "Task 1", 1000, NULL, 3, NULL);
 
-	/* Start the scheduler to start the tasks executing. */
-	vTaskStartScheduler();	
+	/* Tarefa de prioridade 2 */
+	xTaskCreate(vTask_2, "Task 2", 1000, NULL, 2, &xTask2Handle);
+	
+	/* Agendador para iniciar tarefas a serem executadas. */
+	vTaskStartScheduler();
 
-	/* The following line should never be reached because vTaskStartScheduler() 
-	will only return if there was not enough FreeRTOS heap memory available to
-	create the Idle and (if configured) Timer tasks.  Heap management, and
-	techniques for trapping heap exhaustion, are described in the book text. */
-	for( ;; );
+	/*Loop Infinito */
+	for (;; );
 	return 0;
 }
 /*-----------------------------------------------------------*/
 
-void vTask1( void *pvParameters )
-{
-const char *pcTaskName = "Task 1 is running\r\n";
-volatile uint32_t ul;
-
-	/* As per most tasks, this task is implemented in an infinite loop. */
-	for( ;; )
-	{
-		/* Print out the name of this task. */
-		vPrintString( pcTaskName );
-
-		/* Delay for a period. */
-		for( ul = 0; ul < mainDELAY_LOOP_COUNT; ul++ )
-		{
-			/* This loop is just a very crude delay implementation.  There is
-			nothing to do in here.  Later exercises will replace this crude
-			loop with a proper delay/sleep function. */
-		}
-	}
-}
-/*-----------------------------------------------------------*/
-
-void vTask2( void *pvParameters )
-{
-const char *pcTaskName = "Task 2 is running\r\n";
-volatile uint32_t ul;
-
-	/* As per most tasks, this task is implemented in an infinite loop. */
-	for( ;; )
-	{
-		/* Print out the name of this task. */
-		vPrintString( pcTaskName );
-
-		/* Delay for a period. */
-		for( ul = 0; ul < mainDELAY_LOOP_COUNT; ul++ )
-		{
-			/* This loop is just a very crude delay implementation.  There is
-			nothing to do in here.  Later exercises will replace this crude
-			loop with a proper delay/sleep function. */
-		}
-	}
-}
 
 
+
+/*Fun√ß√£o para  ... */
